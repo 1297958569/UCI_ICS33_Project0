e687624281c524c500cfe15cf459da34f6ce6a7c
Wenxuan Chen <wenxuc3@uci.edu>
2023-10-06 19:28:18 -0700

    Initially, the number of queens is zero We will need to import namedTuples and several manually set Errors that are to be ignored to the unittest file in order for further manipulation.
    Secondly, we are making a unittest class that includes and contains multiple tests to testify and verify if certain codes within the main code are executing correctly. For instance, as we check for the original clear board, there should not be any things that is placed on the board, so, we testify whether a plot is clear by using assertEqual function.
    
    The functions that we are testifying includes:
    -Plot is empty
    -Whether an object is successfully added to the board or not
    -Whether an object is successfully removed or not
    -Whether is an object that is cued to be removed has successfully executed ot not
    -Testify is the queen is unsafe according to its locations with 2 different tests.
    ...
    
    And Eventually executing the code using the __name__ == '__main__' function to see if the unittest itself and its corresponding main codes has worked out successfully or not.

--- queens.py
+++ queens.py
@@ -63,44 +63,59 @@ class QueensState:
     """Immutably represents the state of a chessboard being used to assist in
     solving the n-queens problem."""
 
+
+
     def __init__(self, rows: int, columns: int):
         """Initializes the chessboard to have the given numbers of rows and columns,
         with no queens occupying any of its cells."""
-        pass
-
+        self._rows = rows
+        self._columns = columns
+        self._board = [[0] * columns for _ in range(rows)]
 
     def queen_count(self) -> int:
         """Returns the number of queens on the chessboard."""
-        return 0
-
+        return sum(sum(row) for row in self._board)
 
     def queens(self) -> list[Position]:
         """Returns a list of the positions in which queens appear on the chessboard,
         arranged in no particular order."""
-        pass
-
+        return [Position(i, j) for i in range(self._rows) for j in range(self._columns) if self._board[i][j] == 1]
 
     def has_queen(self, position: Position) -> bool:
         """Returns True if a queen occupies the given position on the chessboard, or
         False otherwise."""
-        pass
-
+        return self._board[position.row][position.column] == 1
 
     def any_queens_unsafe(self) -> bool:
         """Returns True if any queens on the chessboard are unsafe (i.e., they can
         be captured by at least one other queen on the chessboard), or False otherwise."""
-        pass
+        queens = self.queens()
+        for i in range(len(queens)):
+            for j in range(i + 1, len(queens)):
+                py1, py2 = queens[i], queens[j]
+                if py1.row == py2.row or py1.column == py2.column or abs(
+                        py1.row - py2.row) == abs(py1.column - py2.column):
+                    return True
+        return False
 
-
-    def with_queens_added(self, positions: list[Position]) -> Self:
+    def with_queens_added(self, positions: list[Position]) -> 'QueensState':
         """Builds a new QueensState with queens added in the given positions.
-        Raises a DuplicateQueenException when there is already a queen in at
+        Raises a DuplicateQueenError when there is already a queen in at
         least one of the given positions."""
-        pass
+        new_board = [row[:] for row in self._board]
+        for pos in positions:
+            if self.has_queen(pos):
+                raise DuplicateQueenError(pos)
+            new_board[pos.row][pos.column] = 1
+        return QueensState(self._rows, self._columns)._replace(_board = new_board)
 
-
-    def with_queens_removed(self, positions: list[Position]) -> Self:
+    def with_queens_removed(self, positions: list[Position]) -> 'QueensState':
         """Builds a new QueensState with queens removed from the given positions.
-        Raises a MissingQueenException when there is no queen in at least one of
+        Raises a MissingQueenError when there is no queen in at least one of
         the given positions."""
-        pass
+        new_board = [row[:] for row in self._board]
+        for pos in positions:
+            if not self.has_queen(pos):
+                raise MissingQueenError(pos)
+            new_board[pos.row][pos.column] = 0
+        return QueensState(self._rows, self._columns)._replace(_board = new_board)
